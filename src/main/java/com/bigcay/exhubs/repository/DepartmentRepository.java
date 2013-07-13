000b7eb7297cf2b83a5a5a32c1763dad41d08866
Leo Bi <biminglei@gmail.com>
2013-07-13 20:28:45 +0800

    minor change

--- src/main/java/com/bigcay/exhubs/repository/DepartmentRepository.java
+++ src/main/java/com/bigcay/exhubs/repository/DepartmentRepository.java
@@ -3,7 +3,6 @@ package com.bigcay.exhubs.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bigcay.exhubs.model.Department;
-import com.bigcay.exhubs.model.Group;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 
